08e192660f2a87aff0208cde388b88a19baf1213
Ben Gubler <nebrelbug@gmail.com>
2025-07-23 16:35:27 -0700

    fix: change route to ambient declaration file

--- packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
+++ packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
@@ -296,7 +296,7 @@ async function startWatcher(
   )
 
   if (opts.nextConfig.experimental.typedRoutes) {
-    const routeTypesFilePath = path.join(distDir, 'types', 'routes.ts')
+    const routeTypesFilePath = path.join(distDir, 'types', 'routes.d.ts')
     await mkdir(path.dirname(routeTypesFilePath), { recursive: true })
 
     const routeTypesManifest = createRouteTypesManifest({
@@ -1069,7 +1069,7 @@ async function startWatcher(
         prevSortedRoutes = sortedRoutes
 
         if (opts.nextConfig.experimental.typedRoutes) {
-          const routeTypesFilePath = path.join(distDir, 'types', 'routes.ts')
+          const routeTypesFilePath = path.join(distDir, 'types', 'routes.d.ts')
           await mkdir(path.dirname(routeTypesFilePath), { recursive: true })
 
           const routeTypesManifest = createRouteTypesManifest({
