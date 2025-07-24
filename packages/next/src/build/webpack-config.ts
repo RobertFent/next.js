795b95d373837a2c873821892cab73090871d4cd
Ben Gubler <nebrelbug@gmail.com>
2025-07-23 18:41:31 -0700

    fix: revert to old file versions (moving edits to typed-links PR)

--- packages/next/src/build/webpack-config.ts
+++ packages/next/src/build/webpack-config.ts
@@ -389,6 +389,7 @@ export default async function getBaseWebpackConfig(
 
   const hasAppDir = !!appDir
   const disableOptimizedLoading = true
+  const enableTypedRoutes = !!config.experimental.typedRoutes && hasAppDir
   const bundledReactChannel = needsExperimentalReact(config)
     ? '-experimental'
     : ''
@@ -2142,6 +2143,7 @@ export default async function getBaseWebpackConfig(
           dev,
           isEdgeServer,
           pageExtensions: config.pageExtensions,
+          typedRoutes: enableTypedRoutes,
           cacheLifeConfig: config.experimental.cacheLife,
           originalRewrites,
           originalRedirects,
